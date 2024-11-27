7ac3e6f3a75979a9f0148d1f818ba413527b894a
RedPill <weic4001@gmail.com>
2024-11-27 13:14:42 +0800

    Initial Commit

---
+++ Ez Unzip/Ez_UnzipApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ez_UnzipApp.swift
+//  Ez Unzip
+//
+//  Created by kkxx on 2024/11/27.
+//
+
+import SwiftUI
+
+@main
+struct Ez_UnzipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
